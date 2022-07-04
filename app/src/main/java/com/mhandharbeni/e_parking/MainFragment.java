d92d68190d46e3b9ebb34c09093b0e3bdae37dc0
Muhammad Handharbeni <muhammad.handharbeni@edgeprop.sg>
2022-07-04 12:49:23 +0700

    initial commit

--- app/src/main/java/com/mhandharbeni/e_parking/MainFragment.java
+++ app/src/main/java/com/mhandharbeni/e_parking/MainFragment.java
@@ -6,10 +6,17 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.mhandharbeni.e_parking.databinding.FragmentMainBinding;
+import com.skydoves.balloon.ArrowOrientation;
+import com.skydoves.balloon.ArrowPositionRules;
+import com.skydoves.balloon.Balloon;
+import com.skydoves.balloon.BalloonAnimation;
+import com.skydoves.balloon.BalloonSizeSpec;
+import com.skydoves.balloon.OnBalloonClickListener;
 
 public class MainFragment extends Fragment {
 
@@ -17,24 +24,44 @@ public class MainFragment extends Fragment {
 
     @Override
     public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
+            @NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
 
         binding = FragmentMainBinding.inflate(inflater, container, false);
+        setupTrigger();
         return binding.getRoot();
 
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    void setupTrigger() {
+        binding.header.menu.setOnClickListener(view -> {
+            Balloon balloon = new Balloon.Builder(requireContext())
+                    .setArrowSize(10)
+                    .setArrowOrientation(ArrowOrientation.TOP)
+                    .setArrowPositionRules(ArrowPositionRules.ALIGN_ANCHOR)
+                    .setArrowPosition(0.5f)
+                    .setWidth(BalloonSizeSpec.WRAP)
+                    .setHeight(BalloonSizeSpec.WRAP)
+                    .setTextSize(15f)
+                    .setCornerRadius(4f)
+                    .setAlpha(0.9f)
+                    .setText("You can access your profile from now on.")
+                    .setBalloonAnimation(BalloonAnimation.FADE)
+                    .setLifecycleOwner(getViewLifecycleOwner())
+                    .build();
+            balloon.showAlignBottom(binding.header.menu);
+        });
     }
 
 }
